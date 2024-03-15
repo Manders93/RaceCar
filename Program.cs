1dd1e82accf84c1b62656f556c4fd00b8d0bb3d1
Manders93 <158100001+Manders93@users.noreply.github.com>
2024-03-14 20:31:49 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using RaceTrack.RaceTrack.Cars;
+using RaceTrack.RaceTrack.Drivers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var raceTrack = new RaceTrack.RaceTrack(5);
+
+            HyundaiSonata HyundaiSonata = new HyundaiSonata();
+            HyundaiSonata.StartEngine();
+            HyundaiSonata.Brake();  
+
+            raceTrack.PositionCars();
+            raceTrack.DriversReady();
+            raceTrack.StartRace();
+
+            for (int i = 0; i < raceTrack.NumberOfLaps; i++)
+            {
+                raceTrack.AnnouncePositions();
+            }
+
+          
+
+            raceTrack.EndRace();
+            raceTrack.AnnounceWinners();
+
+            Amanda Amanda = new Amanda(HyundaiSonata);
+        }
+    }
+}
